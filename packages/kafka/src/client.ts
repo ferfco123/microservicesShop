dc55681a8cfd0cda79320040df4191b299c846b6
ferfco123 <ferfco2000@gmail.com>
2026-08-26 14:19:03 -0300

    feat: add SSL and SASL configuration for Aiven Kafka support

--- packages/kafka/src/client.ts
+++ packages/kafka/src/client.ts
@@ -1,9 +1,31 @@
-import { Kafka } from "kafkajs";
+// import { Kafka } from "kafkajs";
+
+// export const createKafkaClient = (service: string, brokers: string[]) => {
+//   const kafka = new Kafka({
+//     clientId: service,
+//     brokers,
+//   });
+//   return kafka;
+// };
+
+import { Kafka, SASLOptions } from "kafkajs";
 
 export const createKafkaClient = (service: string, brokers: string[]) => {
-  const kafka = new Kafka({
+  const username = process.env.KAFKA_USERNAME;
+  const password = process.env.KAFKA_PASSWORD;
+  const mechanism = (process.env.KAFKA_SASL_MECHANISM || "scram-sha-256") as
+    | "scram-sha-256"
+    | "plain"
+    | "scram-sha-512";
+
+  const sasl: SASLOptions | undefined =
+    username && password ? { mechanism, username, password } : undefined;
+
+  return new Kafka({
     clientId: service,
     brokers,
+
+    ssl: !!username,
+    sasl,
   });
-  return kafka;
 };
